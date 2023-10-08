1d1cf5fff08897e9d03fb1d8b0af759c64c2ca3a
Alex Zhurkevich <pkeprogk@gmail.com>
2023-10-08 19:18:22 +0300

    pages
    Added footer, contacts page, slider on main page

--- creto/store/url.py
+++ creto/store/url.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='main-page'),
+    path('contacts/', views.contacts, name='contacts')
 ]
--- creto/store/views.py
+++ creto/store/views.py
@@ -6,3 +6,8 @@ def index(request):
     bicycles = ProductBicycle.objects.all()
     context = {'bicycles': bicycles}
     return render(request, 'store/index.html', context)
+
+
+def contacts(request):
+
+    return render(request, 'store/contacts.html')
